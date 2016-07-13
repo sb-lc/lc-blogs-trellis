ff3747deced0bdddff8ddb5527aeaa821c120673
Simon Beasley <sbeasley@leicestercollege.ac.uk>
2016-07-13 16:34:31 +0000

    temporarily switch on debugging

--- site/config/environments/staging.php
+++ site/config/environments/staging.php
@@ -1,7 +1,16 @@
 <?php
 /** Staging */
 ini_set('display_errors', 0);
-define('WP_DEBUG_DISPLAY', false);
-define('SCRIPT_DEBUG', false);
+
+define('WP_DEBUG_DISPLAY', true); #siedev # change back to false
+define('SCRIPT_DEBUG', true); #siedev # change back to false
 /** Disable all file modifications including updates and update notifications */
 define('DISALLOW_FILE_MODS', true);
+
+
+#SIEDEV - CONSTANTS ADDED
+
+define('WP_DEBUG_LOG', true);
+define('WP_DEBUG_DISPLAY', true);
+define('ROOT_DIR', $root_dir);
+
