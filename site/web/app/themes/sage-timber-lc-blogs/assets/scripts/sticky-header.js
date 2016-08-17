3a99b519712662f54fa747e518f3c27fef177e8b
Simon Beasley <sbeasley@leicestercollege.ac.uk>
2016-08-17 11:15:29 +0000

    failed attempt to set up scroll triggers in theme

---
+++ site/web/app/themes/sage-timber-lc-blogs/assets/scripts/sticky-header.js
@@ -0,0 +1,18 @@
+(function($) {
+
+	console.log( 'sticky-header.js' );
+
+    var s = skrollr.init({
+        edgeStrategy: 'set',
+        forceHeight: false,
+        smoothScrolling: false,
+        easing: {
+            WTF: Math.random,
+            inverted: function(p) {
+                return 1-p;
+            }
+        }
+    });
+
+
+})(jQuery);
