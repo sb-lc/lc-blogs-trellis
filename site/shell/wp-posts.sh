10d047b811b3d93c0d7f38f0575f2dd3a62c607f
Simon <sbeasley@leicestercollege.ac.uk>
2016-08-30 09:39:47 +0100

    shell tasks setup

--- site/shell/wp-posts.sh
+++ site/shell/wp-posts.sh
@@ -3,7 +3,7 @@
 
 HOME_PAGE_ID=`sudo -u vagrant -i -- wp --path=$1 post create --porcelain --post_type=page --post_title='Home' --post_status='publish'`
 
-sudo -u vagrant -i -- wp --path=$1 post delete $(sudo -u vagrant -i -- wp post list --post_type=page --posts_per_page=1 --post_status=publish --pagename="sample-page" --field=ID --format=ids)
+sudo -u vagrant -i -- wp --path=$1 post delete $(sudo -u vagrant -i -- wp --path=$1 post list --post_type=page --posts_per_page=1 --post_status=publish --pagename="sample-page" --field=ID --format=ids)
 
 sudo -u vagrant -i -- wp --path=$1 option update show_on_front 'page'
 sudo -u vagrant -i -- wp --path=$1 option update page_on_front $HOME_PAGE_ID
